8a4f5461baae3125ba362da4e747abc743d144b7
Zuzana <z.dankovcikova@gmail.com>
2015-05-14 23:48:40 +0200

    Added date of birth dropdowns and validation.

--- cv-generator/src/main/webapp/js/edit.js
+++ cv-generator/src/main/webapp/js/edit.js
@@ -1,9 +1,38 @@
 var editApp = angular.module('editApp', []);
 
+editApp.controller('DateController', ['$scope', function ($scope) {
+        $scope.totaldays = 31;
+        $scope.totalmonths = 12;
+        $scope.totalyears = 150;
+
+        $scope.days = [];
+        for (var i = 0; i < $scope.totaldays; i += 1) {
+            $scope.days.push(i + 1);
+        }
+
+        $scope.months = [];
+        for (var i = 0; i < $scope.totalmonths; i += 1) {
+            $scope.months.push(i + 1);
+        }
+
+        $scope.years = [];
+        var currentYear = new Date().getFullYear();
+        for (var i = currentYear; i > currentYear - $scope.totalyears; i--) {
+            $scope.years.push(i - 1);
+        }
+    }]);
+
 editApp.controller('FormController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
         $scope.showErrors = false;
         $scope.submit = function () {
             if ($scope.userForm.$valid) {
+                if (!checkDate()) {
+                    $window.alert("Come on, only Chuck Norris can be born on "
+                            + $scope.data['personal-details']['date-of-birth'].day + "."
+                            + $scope.data['personal-details']['date-of-birth'].month + "."
+                            + $scope.data['personal-details']['date-of-birth'].year + "!");
+                    return;
+                }
                 $scope.showErrors = false;
                 var data = {};
                 data["curriculum-vitae"] = $scope.data;
@@ -27,6 +56,28 @@ editApp.controller('FormController', ['$scope', '$window', '$http', function ($s
                 $window.alert("Please fill in all requided fields. If you want to leave somethig blank, press 'X' on the right to delete the group.");
             }
         };
+
+        var checkDate = function () {
+            var d = $scope.data['personal-details']['date-of-birth'].day;
+            var m = $scope.data['personal-details']['date-of-birth'].month;
+            var y = $scope.data['personal-details']['date-of-birth'].year;
+
+            return m > 0 && m <= 12 && d > 0 && d <= daysInMonth(m, y);
+        };
+
+        function daysInMonth(m, y) { // m is 1..12
+            switch (m) {
+                case 2 :
+                    return (y % 4 == 0 && y % 100) || y % 400 == 0 ? 29 : 28;
+                case 9 :
+                case 4 :
+                case 6 :
+                case 11 :
+                    return 30;
+                default :
+                    return 31;
+            }
+        }
 
         $scope.createBlankForm = function () {
             $scope.data = {};
