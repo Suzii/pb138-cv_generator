eac537925f8bae669c18d762343fd8a586793789
Jozii <jozefzivcic@gmail.com>
2015-05-20 18:18:14 +0200

    Added test file

---
+++ cv-generator/src/main/java/cz/muni/fi/pb138/cv/test/XMLToTeXTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.cv.test;
+
+import org.junit.Before;
+
+/**
+ *
+ * @author Jozef Živčic
+ */
+public class XMLToTeXTest {
+    @Before
+    public void setUp() {
+      
+    }
+}
