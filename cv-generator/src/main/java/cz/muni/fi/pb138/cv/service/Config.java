9e4b48e4660cc4bb6319638d284da8379fd2f180
Peter <peter202svk@gmail.com>
2015-05-21 16:40:29 +0200

    add xmlunit

--- cv-generator/src/main/java/cz/muni/fi/pb138/cv/service/Config.java
+++ cv-generator/src/main/java/cz/muni/fi/pb138/cv/service/Config.java
@@ -12,6 +12,7 @@ package cz.muni.fi.pb138.cv.service;
 public class Config {
         //musi to byt na takejto absolutnej ceste, lebo inak by sa k tomu nedalo pristupit normalne (servlet bezi v inom prieciku)
     public static final String SAMPLE_JSON_PATH = "C:/sample-data.json";
+    public static final String SAMPLE_XML_PATH = "C:/sample-cv.xml";
     public static final String SAMPLE_PDF_PATH = "C:/cv_9.pdf";
     public static final String DIRECTORY = "C:/pb138-database";
     public static final String LOGINS = "C:/pb138-database/users.xml";
