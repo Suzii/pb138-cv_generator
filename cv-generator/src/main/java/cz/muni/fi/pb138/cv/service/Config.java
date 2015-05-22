52354ad37f5e5d78294c0dae160f9e0e9fd47cef
Zuzana <z.dankovcikova@gmail.com>
2015-05-22 13:51:40 +0200

    Deleted unused code

--- cv-generator/src/main/java/cz/muni/fi/pb138/cv/service/Config.java
+++ cv-generator/src/main/java/cz/muni/fi/pb138/cv/service/Config.java
@@ -15,19 +15,19 @@ import java.util.Properties;
  */
 public class Config {
 
-    //musi to byt na takejto absolutnej ceste, lebo inak by sa k tomu nedalo
-    //pristupit normalne (servlet bezi v inom prieciku)
     public static final String SAMPLE_JSON_PATH = "src\\main\\resources\\TestData\\pb138-database\\sample-data.json";
     public static final String SAMPLE_XML_PATH = "src\\main\\resources\\TestData\\pb138-database\\sample-cv.xml";
     public static final String TEST_DB = "src\\main\\resources\\TestData\\pb138-database";
+    public static final String SAMPLE_PDF_PATH = TEST_DB + "\\sample.pdf";
 
-    public static final String SAMPLE_PDF_PATH = "C:/cv_9.pdf";
+    @Deprecated
     public static final String DIRECTORY = "C:/pb138-database";
+    @Deprecated
     public static final String LOGINS = "C:/pb138-database/users.xml";
+    @Deprecated
     public static final String DBUTIL = "C:/pb138-database/Utils";
-    // public static final String PATHTOLATEXBIN="C:\\Programy\\MiKTeX 2.9\\miktex\\bin\\";  //Cypo
+    @Deprecated
     public static final String PATHTOLATEXBIN = "C:\\Program Files (x86)\\MiKTeX 2.9\\miktex\\bin\\"; //Suzii
-    // public static final String XMLVALIDATIONFILE;    TODO
 
     public String getLatexFolder() {
         Properties properties = new Properties();
